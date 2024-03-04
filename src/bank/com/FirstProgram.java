e941ca9517be08724d577f7ea633947023bd0b46
Thanusuyaa <tanuselvakumar197@gmail.com>
2024-03-04 11:17:03 +0530

    first commit

---
+++ src/bank/com/FirstProgram.java
@@ -0,0 +1,13 @@
+package bank.com;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int no1=10;
+    int no2=20;
+    int total=no1+no2;
+    System.out.println(total);
+	}
+
+}
